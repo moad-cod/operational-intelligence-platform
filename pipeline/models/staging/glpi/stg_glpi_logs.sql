046c77b892bf1d4e759cbaf6cb823cdedaf6bf2a
Mouad El Baz <elbaz.ouad1249@gmail.com>
2026-04-30 19:37:33 +0100

    Implement stg_glpi_logs.sql

---
+++ pipeline/models/staging/glpi/stg_glpi_logs.sql
@@ -0,0 +1,54 @@
+WITH source AS (
+
+    SELECT *
+    FROM {{ ref('base_glpi_logs') }}
+
+),
+
+renamed AS (
+
+    SELECT
+        -- 🔑 identifiers
+        id AS log_id,
+        items_id AS entity_id,
+        itemtype AS entity_type,
+
+        -- link info
+        itemtype_link AS linked_entity_type,
+        linked_action,
+
+        -- user
+        user_name,
+
+        -- time
+        CAST(date_mod AS DATETIME) AS updated_at,
+
+        -- change tracking
+        id_search_option AS field_id,
+        old_value,
+        new_value,
+
+        -- derived
+        CASE
+            WHEN (old_value IS NULL OR old_value = '') 
+                AND (new_value IS NOT NULL AND new_value != '') 
+                THEN 'created'
+
+            WHEN (old_value IS NOT NULL AND old_value != '') 
+                AND (new_value IS NULL OR new_value = '') 
+                THEN 'deleted'
+
+            WHEN old_value != new_value 
+                THEN 'updated'
+
+            ELSE 'no_change'
+        END AS change_type,
+
+        -- source
+        year AS source_year
+
+    FROM source
+
+)
+
+SELECT * FROM renamed
