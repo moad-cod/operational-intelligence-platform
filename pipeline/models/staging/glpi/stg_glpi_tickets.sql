6918690c82faf87c498307e4f4a76a7ce565cec8
Mouad El Baz <elbaz.ouad1249@gmail.com>
2026-05-02 08:38:11 +0100

    fix(messing): columns names stg_glpi_tickets

--- pipeline/models/staging/glpi/stg_glpi_tickets.sql
+++ pipeline/models/staging/glpi/stg_glpi_tickets.sql
@@ -1,7 +1,7 @@
 WITH source AS (
 
     SELECT *
-    FROM {{ ref('base_glpi_ticketfollowups') }}
+    FROM {{ ref('base_glpi_tickets') }}
 
 ),
 
@@ -9,26 +9,41 @@ cleaned AS (
 
     SELECT
         -- Composite PK (because ids repeat across years)
-        CONCAT(year, '_', id) AS ticket_followup_pk,
+        CONCAT(year, '_', id) AS ticket_pk,
 
         -- Business keys
-        id AS followup_id,
-        tickets_id AS ticket_id,
+        id AS ticket_id,
 
         -- Dates
-        date AS created_at,
+        CASE
+            WHEN date IS NULL THEN NULL
+            WHEN CAST(date AS CHAR) = '0000-00-00 00:00:00' THEN NULL
+            ELSE date
+        END AS created_at,
+        CASE
+            WHEN solvedate IS NULL THEN NULL
+            WHEN CAST(solvedate AS CHAR) = '0000-00-00 00:00:00' THEN NULL
+            WHEN solvedate < date THEN NULL
+            ELSE solvedate
+        END AS solved_at,
+        CASE
+            WHEN closedate IS NULL THEN NULL
+            WHEN CAST(closedate AS CHAR) = '0000-00-00 00:00:00' THEN NULL
+            WHEN closedate < date THEN NULL
+            ELSE closedate
+        END AS closed_at,
 
-        -- Dimensions
-        users_id AS user_id,
-        requesttypes_id AS request_type_id,
+        -- Ticket status and lifecycle flags
+        status,
+        is_deleted,
 
-        -- Content
-        content,
+        -- SLA metrics
+        waiting_duration,
+        close_delay_stat,
+        solve_delay_stat,
+        takeintoaccount_delay_stat,
 
-        -- Flags
-        is_private,
-
-        -- Metadata
+        -- Source tracking
         year AS source_year
 
     FROM source
